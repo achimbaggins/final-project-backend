f5a0319ff110722249244dabbe8a0ffe13c420ab
achimbaggins <achim_baggins@yahoo.com>
2017-11-01 14:55:53 +0700

    adding station model

--- routes/station.js
+++ routes/station.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const Station = require('../models/Station');
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
