bac6ec034792eda583aa55508d46f845e48e7f4f
achimbaggins <achim_baggins@yahoo.com>
2017-11-01 13:07:16 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', index);
+app.use('/users', users);
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.send('something error bray');
+});
+
+module.exports = app;
