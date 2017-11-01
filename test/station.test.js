01c5c4648d88056922f14218654bc2d671d4e745
Irianto <irianto223@gmail.com>
2017-11-01 15:56:16 +0700

    get all station testing DONE

--- test/station.test.js
+++ test/station.test.js
@@ -14,10 +14,19 @@ var newStation = {
   description: 'dummy_station description bla bla bla'
 }
 
+var newStation2 = {
+  coordinate: [0,0],
+  stationName: 'dummy_station_2',
+  imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Harmoni_Central_Busway_Transjakarta_2.JPG',
+  description: 'dummy_station_2 description bla bla bla'
+}
+
 describe ('CREATE NEW STATION: ', () => {
 
   afterEach(done => {
-    Station.remove()
+    Station.remove({
+      _id: newStation._id
+    })
     .then(response => done())
     .catch(err => console.log(err))
   })
@@ -27,6 +36,7 @@ describe ('CREATE NEW STATION: ', () => {
     .post('/stations')
     .send(newStation)
     .end((err, response) => {
+      newStation._id = response.body.data._id
       response.status.should.equal(200)
       done()
     })
@@ -37,6 +47,7 @@ describe ('CREATE NEW STATION: ', () => {
     .post('/stations')
     .send(newStation)
     .end((err, response) => {
+      newStation._id = response.body.data._id
       response.body.data.should.be.an('object')
       done()
     })
@@ -47,6 +58,7 @@ describe ('CREATE NEW STATION: ', () => {
     .post('/stations')
     .send(newStation)
     .end((err, response) => {
+      newStation._id = response.body.data._id
       response.body.data.coordinate.should.equal('testing title')
       done()
     })
@@ -57,6 +69,7 @@ describe ('CREATE NEW STATION: ', () => {
     .post('/stations')
     .send(newStation)
     .end((err, response) => {
+      newStation._id = response.body.data._id
       response.body.data.stationName.should.equal('dummy_station')
       done()
     })
@@ -67,6 +80,7 @@ describe ('CREATE NEW STATION: ', () => {
     .post('/stations')
     .send(newStation)
     .end((err, response) => {
+      newStation._id = response.body.data._id
       response.body.data.imageUrl.should.equal('https://upload.wikimedia.org/wikipedia/commons/0/03/Harmoni_Central_Busway_Transjakarta_1.JPG')
       done()
     })
@@ -77,6 +91,7 @@ describe ('CREATE NEW STATION: ', () => {
     .post('/stations')
     .send(newStation)
     .end((err, response) => {
+      newStation._id = response.body.data._id
       response.body.data.description.should.equal('dummy_station description bla bla bla')
       done()
     })
@@ -88,6 +103,7 @@ describe ('CREATE NEW STATION: ', () => {
     .post('/stations')
     .send(newStation)
     .end((err, response) => {
+      newStation._id = response.body.data._id
       response.body.errors.coordinate.message.should.equal('Path `coordinate` is required.')
       done()
     })
@@ -128,14 +144,12 @@ describe ('CREATE NEW STATION: ', () => {
 
 })
 
-var id_station_dummy = null
-
 describe('DELETE STATION: ', () => {
 
   beforeEach(done => {
     Station.create(newStation)
     .then(response => {
-      id_station_dummy = response._id
+      newStation._id = response._id
       done()
     })
     .catch(err => console.log(err))
@@ -143,7 +157,7 @@ describe('DELETE STATION: ', () => {
 
   it ('should return response status = 200', (done) => {
     chai.request(app)
-    .delete(`/stations/${id_station_dummy}`)
+    .delete(`/stations/${newStation._id}`)
     .end((err, response) => {
       response.status.should.equal(200)
       done()
@@ -152,16 +166,79 @@ describe('DELETE STATION: ', () => {
 
   it ('expect response2.body to not have property "data"', (done) => {
     chai.request(app)
-    .delete(`/stations/${id_station_dummy}`)
+    .delete(`/stations/${newStation._id}`)
     .end((err, response) => {
 
       chai.request(app)
-      .get(`/stations/${id_station_dummy}`)
+      .get(`/stations/${newStation._id}`)
       .end((err2, response2) => {
         expect(response2.body).to.not.have.property('data');
         done()
       })
 
+    })
+  })
+
+})
+
+describe('GET ALL STATIONS: ', () => {
+
+  beforeEach(done => {
+    Station.create(newStation)
+    .then(response => {
+      newStation._id = response._id
+
+      Station.create(newStation2)
+      .then(response2 => {
+        newStation2._id = response2._id
+        done()
+      })
+      .catch(err2 => console.log(err2))
+
+    })
+    .catch(err => console.log(err))
+  })
+
+  afterEach(done => {
+    Station.remove({
+      _id: newStation._id
+    })
+    .then(response => {
+
+      Station.remove({
+        _id: newStation2._id
+      })
+      .then(response2 => done())
+      .catch(err2 => console.log(err2))
+
+    })
+    .catch(err => console.log(err))
+  })
+
+  it ('should return response status = 200', (done) => {
+    chai.request(app)
+    .get('/stations')
+    .end((err, response) => {
+      response.status.should.equal(200)
+      done()
+    })
+  })
+
+  it ('should return response.body.data as an "array"', (done) => {
+    chai.request(app)
+    .get('/stations')
+    .end((err, response) => {
+      response.body.data.should.be.an('array')
+      done()
+    })
+  })
+
+  it ('should return response.body.data.length = 2', (done) => {
+    chai.request(app)
+    .get('/stations')
+    .end((err, response) => {
+      response.body.data.length.should.equal(2)
+      done()
     })
   })
 
