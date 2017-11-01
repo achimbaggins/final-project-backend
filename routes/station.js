ae270a057e0759341e274268e511c1c618b86f59
achimbaggins <achim_baggins@yahoo.com>
2017-11-01 15:42:55 +0700

    edit station router

--- routes/station.js
+++ routes/station.js
@@ -3,5 +3,9 @@ var router = express.Router();
 const stationControl = require('../controllers/stationController');
 
 router.get('/', stationControl.all);
+router.post('/', stationControl.create);
+router.get('/:id', stationControl.byId);
+router.put('/:id', stationControl.put);
+router.delete('/:id', stationControl.remove);
 
 module.exports = router;
