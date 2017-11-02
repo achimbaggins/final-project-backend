1a1c6a05f566f5a29dcd1db542be10e9270231ed
Irianto <irianto223@gmail.com>
2017-11-02 10:24:49 +0700

    change schema

--- models/Station.js
+++ models/Station.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 const stationSchema = new Schema ({
-  coordinate: [{type: String}],
+  coordinate: {
+    type: String,
+    required: true
+  },
   stationName: {
     type: String,
     required: true
--- test/station.test.js
+++ test/station.test.js
@@ -8,14 +8,14 @@ const Station = require('../models/Station')
 chai.use(chaiHttp)
 
 var newStation = {
-  coordinate: [0,0],
+  coordinate: JSON.stringify({lat: 0, lng: 0}),
   stationName: 'dummy_station',
   imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/03/Harmoni_Central_Busway_Transjakarta_1.JPG',
   description: 'dummy_station description bla bla bla'
 }
 
 var newStation2 = {
-  coordinate: [1,1],
+  coordinate: JSON.stringify({lat: 1, lng: 1}),
   stationName: 'dummy_station_2',
   imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Harmoni_Central_Busway_Transjakarta_2.JPG',
   description: 'dummy_station_2 description bla bla bla'
@@ -53,14 +53,15 @@ describe ('TESTING CREATE STATION DATA: ', () => {
     })
   })
 
-  it ('should return response.body.coordinate = "[0,0]"', (done) => {
+  it (`should return response.body.coordinate = "${newStation.coordinate}"`, (done) => {
     chai.request(app)
     .post('/stations')
     .send(newStation)
     .end((err, response) => {
       newStation._id = response.body._id
-      response.body.coordinate[0].should.equal('0')
-      response.body.coordinate[1].should.equal('0')
+      let coordinate = JSON.parse(response.body.coordinate)
+      coordinate.lat.should.equal(0)
+      coordinate.lng.should.equal(0)
       done()
     })
   })
@@ -259,8 +260,9 @@ describe('TESTING GET ALL STATIONS DATA: ', () => {
     chai.request(app)
     .get(`/stations`)
     .end((err, response) => {
-      response.body[0].coordinate[0].should.equal('0')
-      response.body[0].coordinate[1].should.equal('0')
+      let coordinate = JSON.parse(response.body[0].coordinate)
+      coordinate.lat.should.equal(0)
+      coordinate.lng.should.equal(0)
       done()
     })
   })
@@ -296,8 +298,9 @@ describe('TESTING GET ALL STATIONS DATA: ', () => {
     chai.request(app)
     .get(`/stations`)
     .end((err, response) => {
-      response.body[1].coordinate[0].should.equal('1')
-      response.body[1].coordinate[1].should.equal('1')
+      let coordinate = JSON.parse(response.body[1].coordinate)
+      coordinate.lat.should.equal(1)
+      coordinate.lng.should.equal(1)
       done()
     })
   })
@@ -377,8 +380,9 @@ describe('TESTING GET ONE STATION DATA: ', () => {
     chai.request(app)
     .get(`/stations/${newStation._id}`)
     .end((err, response) => {
-      response.body.coordinate[0].should.equal('0')
-      response.body.coordinate[1].should.equal('0')
+      let coordinate = JSON.parse(response.body.coordinate)
+      coordinate.lat.should.equal(0)
+      coordinate.lng.should.equal(0)
       done()
     })
   })
@@ -447,7 +451,7 @@ describe('TESTING UPDATE STATION DATA: ', () => {
     })
   })
 
-  it ('should return response2.body.coordinate = "[1,1]"', (done) => {
+  it (`should return response2.body.coordinate = "${newStation2.coordinate}"`, (done) => {
     delete newStation2._id
     chai.request(app)
     .put(`/stations/${newStation._id}`)
@@ -457,8 +461,9 @@ describe('TESTING UPDATE STATION DATA: ', () => {
       chai.request(app)
       .get(`/stations/${newStation._id}`)
       .end((err2, response2) => {
-        response2.body.coordinate[0].should.equal('1')
-        response2.body.coordinate[1].should.equal('1')
+        let coordinate = JSON.parse(response2.body.coordinate)
+        coordinate.lat.should.equal(1)
+        coordinate.lng.should.equal(1)
         done()
       })
 
